56cb53ec093b914d8a8dbb86f36bf47e17a02597
Colby M. White <colbywhite@users.noreply.github.com>
2021-07-23 14:37:42 -0500

    fix: add null check to html min

--- .eleventy.js
+++ .eleventy.js
@@ -64,7 +64,7 @@ module.exports = function(eleventyConfig) {
 
   // Minify HTML output
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
+    if (outputPath && outputPath.indexOf(".html") > -1) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
