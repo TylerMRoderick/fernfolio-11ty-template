ab973ce38151bb5fda09fce6463305b56686fd9e
Tyler M. Roderick <tyler@cloudspot.io>
2022-08-04 13:50:41 -0700

    fix: modify date format

--- .eleventy.js
+++ .eleventy.js
@@ -39,7 +39,7 @@ module.exports = function(eleventyConfig) {
 
   // Date formatting (human readable)
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj).toFormat("DD");
   });
 
   // Date formatting (machine readable)
