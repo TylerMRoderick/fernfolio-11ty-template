37ddfa3ab4fa5fc2d069eb3f5d52d22b41034203
Tyler M. Roderick <tyler@cloudspot.io>
2022-08-04 14:50:37 -0700

    chore: lint inline.js

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,8 +1,8 @@
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
+  window.netlifyIdentity.on('init', (user) => {
     if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
       });
     }
   });
