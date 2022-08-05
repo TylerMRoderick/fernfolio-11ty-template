efbaa88444df656ddaad09b011b091430235e2cf
Tyler M. Roderick <tyler@cloudspot.io>
2022-08-04 17:34:46 -0700

    chore: simplify eleventy return to exclude default options

--- .eleventy.js
+++ .eleventy.js
@@ -15,8 +15,7 @@ module.exports = function (eleventyConfig) {
   // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // Merge data instead of overriding
-  // https://www.11ty.dev/docs/data-deep-merge/
+  // Merge data instead of overriding https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
 
   // Add support for maintenance-free post authors
@@ -93,22 +92,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary('md', markdownLibrary);
 
   return {
-    templateFormats: ['md', 'njk', 'html', 'liquid'],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: '/',
-
-    markdownTemplateEngine: 'liquid',
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     dir: {
       input: 'src',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
     },
   };
 };
