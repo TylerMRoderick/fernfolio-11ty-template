2a3c3efc010748e44b014dc87590c2aedbfbaaf5
Tyler M. Roderick <tyler@cloudspot.io>
2022-08-10 16:24:02 -0700

    fix: remove hr from cms post

--- src/admin/preview-templates/post.js
+++ src/admin/preview-templates/post.js
@@ -17,12 +17,11 @@ const Post = createClass({
               <time
                 >${format(
                   entry.getIn(['data', 'date'], new Date()),
-                  'dd MMM, yyyy'
+                  'MMMM dd, yyyy'
                 )}</time
               >
             </small>
           </p>
-          <hr>
 
           <p>${entry.getIn(['data', 'summary'], '')}</p>
 
