648f7d61088a09a49bcdfa49ce2acf9222158146
Tyler M. Roderick <tyler@cloudspot.io>
2022-08-10 16:22:07 -0700

    fix: reset date format

--- src/admin/preview-templates/post.js
+++ src/admin/preview-templates/post.js
@@ -17,7 +17,7 @@ const Post = createClass({
               <time
                 >${format(
                   entry.getIn(['data', 'date'], new Date()),
-                  'MMMM dd, yyyy'
+                  'dd MMM, yyyy'
                 )}</time
               >
             </small>
