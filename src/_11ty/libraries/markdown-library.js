6537acc04db142882269c18906221ab9b66e73f4
Tyler M. Roderick <tyler@cloudspot.io>
2022-10-09 10:53:59 -0700

    feat: add responsive image support to markdown

--- src/_11ty/libraries/markdown-library.js
+++ src/_11ty/libraries/markdown-library.js
@@ -1,13 +1,68 @@
 let markdownIt = require('markdown-it');
 let markdownItAnchor = require('markdown-it-anchor');
+const Image = require('@11ty/eleventy-img');
 
 // Customize Markdown library and settings
-let markdownLibrary = markdownIt({
+let markdown = markdownIt({
   html: true,
   breaks: true,
   linkify: true
-}).use(markdownItAnchor,  {
-  permalink: false,
-});
+}).use(markdownItAnchor, { permalink: false });
 
-module.exports = markdownLibrary;
+
+// Add responsive image suppport to markdown files
+// borrowed from: https://tomichen.com/blog/posts/20220416-responsive-images-in-markdown-with-eleventy-image
+markdown.renderer.rules.image = function (tokens, idx) {
+  const token = tokens[idx];
+  let imgSrc = token.attrGet('src');
+  const imgAlt = token.content;
+  const imgTitle = token.attrGet('title');
+
+  const htmlOpts = { alt: imgAlt, loading: 'lazy', decoding: 'async' };
+
+  if (imgSrc.startsWith('/assets')) {
+    imgSrc = 'src' + imgSrc;
+  }
+
+  const parsed = (imgTitle || '').match(
+    /^(?<skip>@skip(?:\[(?<width>\d+)x(?<height>\d+)\])? ?)?(?:\?\[(?<sizes>.*?)\] ?)?(?<caption>.*)/
+  ).groups;
+
+  // handle skipped and remote images
+  if (parsed.skip || imgSrc.startsWith('http')) {
+    const options = { ...htmlOpts };
+    const metadata = { jpeg: [{ url: imgSrc }] };
+
+    if (parsed.sizes) {
+      options.sizes = parsed.sizes;
+    }
+
+    if (parsed.width && parsed.height) {
+      metadata.jpeg[0].width = parsed.width
+      metadata.jpeg[0].height = parsed.height
+    }
+
+    return Image.generateHTML(metadata, options);
+  }
+
+  const widths = [250, 316, 426, 460, 580, 768];
+  const imgOpts = {
+    widths: widths
+      .concat(widths.map((w) => w * 2)) // generate 2x sizes
+      .filter((v, i, s) => s.indexOf(v) === i), // dedupe
+    formats: ['webp', 'jpeg'],
+    urlPath: '/assets/img/',
+    outputDir: './_site/assets/img/',
+  };
+
+  Image(imgSrc, imgOpts);
+
+  const metadata = Image.statsSync(imgSrc, imgOpts);
+
+  return Image.generateHTML(metadata, {
+    sizes: parsed.sizes || '(max-width: 768px) 100vw, 768px',
+    ...htmlOpts
+  });
+}
+
+module.exports = markdown;
