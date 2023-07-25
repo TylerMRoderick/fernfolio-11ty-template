9c53d2b4272c0374026f002456b29c64a01595f6
Kobus Post <kobuspost@gmail.com>
2023-07-25 17:32:59 +0200

    Toggle dark mode

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,16 @@
 const Turbolinks = require('turbolinks');
 const drawer = require('./drawer');
+const darkMode = require('./dark_mode');
 
 // Initialize Turbolinks
 Turbolinks.start();
 
 // Initialize mobile nav drawer
 drawer();
+
+// Toggle dark mode
+darkMode();
+document.addEventListener("turbolinks:load", darkMode);
 
 // Handle Netlify Identity Login
 if (window.netlifyIdentity) {
